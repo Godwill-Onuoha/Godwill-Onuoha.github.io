09939d8960b18060b987cebdd277127c08a1dba5
Godwill-Onuoha <106790295+Godwill-Onuoha@users.noreply.github.com>
2022-06-03 03:15:17 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const menu_btn = document.querySelector(".menu-btn");
+const navbar = document.querySelector(".navbar");
+
+
+function mobile_menu(){
+    if(navbar.style.right == "-100%"){navbar.style.right = "0"}
+    else{navbar.style.right = "-100%"}
+}
+
+
+window.addEventListener("load",function reveal_hero_text(){
+    let hide1 = document.querySelector(".hide1");
+    let hide2 = document.querySelector(".hide2");
+    let hide3 = document.querySelector(".hide3");
+
+    hide1.classList.add("reveal1");
+    hide2.classList.add("reveal2");
+    setTimeout(()=>{
+        hide3.classList.add("reveal3");},1000)
+})
